567e5592b504addac2fcdfdf594f6b96e2d5be8f
john-smilga <johnsmilga@gmail.com>
2020-10-30 10:40:57 -0700

    changed section titles

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,8 +43,8 @@ const IndexPage = ({
         </a>
       </div>
       <Projects title="DOM projects" projects={domProjects} />
-      <Projects title="first API projects" projects={apiProjects} />
-      <Projects title="ES6 projects" projects={es6Projects} />
+      <Projects title="basic API projects" projects={apiProjects} />
+      <Projects title="ES6 &amp; advanced API" projects={es6Projects} />
       <Projects title="e-commerce project" projects={storeProjects} />
     </Layout>
   )
