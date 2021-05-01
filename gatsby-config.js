776f15c48ab8de56b440762b509fd546790debad
john-smilga <johnsmilga@gmail.com>
2021-05-01 08:31:06 -0700

    removed algolia in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,15 @@
-require('dotenv').config({
+require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
-// const queries = require("./src/constants/algolia")
+
 module.exports = {
   siteMetadata: {
     title: `John Smilga`,
     description: `Vanilla Javascript Projects`,
     author: `@johnsmilga`,
-    twitterUsername: '@john_smilga',
-    image: '/mainBcg.jpeg',
-    siteUrl: 'https://www.johnsmilga.com',
+    twitterUsername: "@john_smilga",
+    image: "/mainBcg.jpeg",
+    siteUrl: "https://www.johnsmilga.com",
   },
   plugins: [
     {
@@ -27,11 +27,11 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-robots-txt',
+      resolve: "gatsby-plugin-robots-txt",
       options: {
-        host: 'https://www.johnsmilga.com',
-        sitemap: 'https://www.johnsmilga.com/sitemap.xml',
-        policy: [{ userAgent: '*', allow: '/' }],
+        host: "https://www.johnsmilga.com",
+        sitemap: "https://www.johnsmilga.com/sitemap.xml",
+        policy: [{ userAgent: "*", allow: "/" }],
       },
     },
     {
@@ -40,12 +40,12 @@ module.exports = {
         fonts: {
           google: [
             {
-              family: 'Montserrat',
-              variants: ['400', '500', '600', '700'],
+              family: "Montserrat",
+              variants: ["400", "500", "600", "700"],
             },
             {
-              family: 'Inconsolata',
-              variants: ['400', '500', '600', '700'],
+              family: "Inconsolata",
+              variants: ["400", "500", "600", "700"],
             },
           ],
         },
